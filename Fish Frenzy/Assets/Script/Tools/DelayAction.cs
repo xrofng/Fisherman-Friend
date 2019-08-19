650a0c4f6b38c4f9455cd996bd8d1d1c65cbcd94
Chayapol Khodkham <xrofng@gmail.com>
2019-08-19 15:12:16 +0700

    feat: market's Boat

---
+++ Fish Frenzy/Assets/Script/Tools/DelayAction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Xrofng
+{
+    [Serializable]
+    public class DelayAction
+    {
+        public Duration Duration;
+        public Action beforeAction;
+        public Action afterAction;
+
+        public IEnumerator coAction()
+        {
+            if(beforeAction != null)
+            {
+                beforeAction();
+            }
+            if (Duration.durationTimeUnit == DurationTimeUnit.Frame)
+            {
+                int frameCount = 0;
+                while (frameCount < Duration.duration)
+                {
+                    yield return new WaitForEndOfFrame();
+                    frameCount += 1;
+                }
+            }
+            if(Duration.durationTimeUnit == DurationTimeUnit.Second)
+            {
+                yield return new WaitForSeconds(Duration.duration);
+            }
+            if (afterAction != null)
+            {
+                afterAction();
+            }
+        }
+    }
+
+}
---
+++ Fish Frenzy/Assets/Script/Tools/Duration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Xrofng
+{
+    public enum DurationTimeUnit
+    {
+        Second,
+        Frame
+    }
+
+    [System.Serializable]
+    public class Duration
+    {
+        public DurationTimeUnit durationTimeUnit;
+        public float duration;
+    }
+}
+
